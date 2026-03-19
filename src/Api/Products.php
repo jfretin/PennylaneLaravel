df393382344f4e73043e62b3c6293d94f4573559
Jonathan Fretin <jonathan.f@mistersmoke.com>
2026-03-19 13:47:38 +0100

    Added list method for resource Products.

--- src/Api/Products.php
+++ src/Api/Products.php
@@ -4,14 +4,57 @@ namespace Ashraam\PennylaneLaravel\Api;
 
 class Products extends BaseApi
 {
+    use Filterable;
+
+    private $filter_fields = [
+        'id',
+        'label',
+        'reference',
+        'external_reference',
+    ];
+
+    private $sort_fields = [
+        'id',
+    ];
+
     /**
-     * List all products
+     * List products.
      *
+     * @param int $page Ignored when a cursor is provided.
+     * @param int $per_page Items per page / cursor page size.
+     * @param array $filters Array of filters.
+     * @param string|null $sort Sort field, optionally prefixed with `-`.
+     * @param string|null $cursor Cursor for V2 pagination.
      * @return array
+     * @author Jonathan F. <jonathan.f@mistersmoke.com>
      */
-    public function list()
+    public function list($page = 1, $per_page = 20, $filters = [], ?string $sort = null, ?string $cursor = null)
     {
-        return $this->requestJson('get', $this->getNamespace() . "products");
+        $filter = $this->get_filters($filters);
+        $sort = $this->get_sort($sort);
+        $query = [];
+        $useCursorPagination = $this->isV2() && $this->uses2026ApiChanges() && func_num_args() >= 5;
+
+        if ($useCursorPagination) {
+            $query['limit'] = $per_page;
+            if ($cursor !== null) {
+                $query['cursor'] = $cursor;
+            }
+        } else {
+            $query['page'] = $page;
+            $query['per_page'] = $per_page;
+        }
+
+        if ($filter != '') {
+            $query['filter'] = $filter;
+        }
+        if ($sort != '') {
+            $query['sort'] = $sort;
+        }
+
+        $query_string = http_build_query($query);
+
+        return $this->requestJson('get', $this->getNamespace() . "products" . ($query_string ? ('?' . $query_string) : ''));
     }
 
 
